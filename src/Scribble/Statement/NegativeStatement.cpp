9787625f4e4c72cf4f897873fb14fcfa5747e3b0
Blake Loring <blake_@parsed.uk>
2014-09-18 23:08:57 +0100

    More statement updates

--- src/Scribble/Statement/NegativeStatement.cpp
+++ src/Scribble/Statement/NegativeStatement.cpp
@@ -22,12 +22,11 @@ NegativeStatement::~NegativeStatement() {
 void NegativeStatement::checkTree(Type* functionType) {
 	exp_->checkTree(functionType);
 
-	if (exp_->type()->type()->getType() == Int) {
-	} else if (exp_->type()->type()->getType() == Float32) {
-	} else {
-		throw StatementException(this, std::string("cannot negate type ") + exp_->type()->type()->getTypeName());
-	}
-
+	StatementAssert(this,
+			exp_->type()->type()->getType() == Int
+					|| exp_->type()->type()->getType() == Float32,
+			std::string("cannot negate type ")
+					+ exp_->type()->type()->getTypeName());
 }
 
 TypeReference NegativeStatement::type() {
