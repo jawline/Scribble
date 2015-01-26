cd2abd740f67eaf6c6e54e92a9f26287dd1f6087
Blake Loring <blake_l@parsed.uk>
2015-01-26 14:45:50 +0000

    Docs & Formatting

--- src/VM/Heap.hpp
+++ src/VM/Heap.hpp
@@ -13,15 +13,12 @@
 #include <types.h>
 #include <map>
 
-//TODO: This will break at 4bn indexs created in a runtime. Do it cleaner
-
 namespace VM {
 
 class Heap {
   private:
     std::vector<VMHeapEntry> heapItems_;
     std::stack<int> unusedIndexs_;
-
     bool lastFlagState_;
 
   public:
@@ -31,26 +28,45 @@ class Heap {
     /**
      * Returns a value > 0 which acts as a reference to an element on the heap.
      */
-
     long allocate(SmartPointer<VMEntryType> type, int size, uint8_t* initial);
 
+    /**
+     * Returns true if the entry given is a valid reference to an entry on the heap
+     */
     bool validReference(long entry);
+    
+    /**
+     * Get the type 
+     */
     SmartPointer<VMEntryType> getType(long entry);
+    
+    /**
+     * Get a smartpointer to the allocation, this will lead to the data persisting after the heap is destroyed
+     */
     SmartPointer<uint8_t> getSmartPointer(long entry);
+    
+    /**
+     * Returns the address in memory of the heap entries data
+     */
     uint8_t* getAddress(long entry);
+    
+    /**
+     * Returns the size in bytes of a heap entry
+     */
     int getSize(long entry);
 
     void flag(long i);
-
     void lock(long i);
     void unlock(long i);
 
     /**
      * This function deletes any unflagged heap elements and then sets flag states for the next garbage collector run
      */
-
     int processUnflagged();
 
+    /**
+     * Returns some debug information about the heap
+     */
     std::string debugState();
 };
 
