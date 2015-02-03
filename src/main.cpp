77eeffaf16dfc36e759e5e2b0feeb44bc26ebfa1
Blake Loring <blake_l@parsed.uk>
2015-02-03 14:32:17 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,25 @@
 /**
  * Entry point for Scribble
- * @author Blake Loring
+ * Blake Loring
  */
-
 #include <iostream>
 #include <algorithm>
 
 /**
  * Version info defines the automatically generated scribble version.
  */
-
 #include <version_info.hpp>
 #include <Scribble/Scribble.hpp>
 
 /**
  * Defines clock
  */
-
 #include <time.h>
 
 /**
  * In this case that the Function files to compile ParserException will be thrown.
  */
-
 #include <Scribble/Parser/ParserException.hpp>
-
 #include <Arguments/ArgumentsParser.hpp>
 
 char const* getCmdOption(char ** begin, char ** end, char const* defaultOption,
@@ -43,7 +38,7 @@ bool cmdOptionExists(char** begin, char** end, const std::string& option) {
     return std::find(begin, end, option) != end;
 }
 
-bool isOption(std::string str) {
+bool isOption(std::string const& str) {
 
     //Check if str starts with -, if it is then its an option
     if (str.compare(0, 1, "-") == 0) {
@@ -80,8 +75,7 @@ void printUsage(char** argv, int argc) {
     char const* exe = argc < 1 ? "scribble" : *argv;
 
     printf("Usage: %s [OPTIONS] file\n", exe);
-    printf(
-        "Options:\n-v --version: version information\n-r --runtime: output the time it takes for a script to execute\n");
+    printf("Options:\n-v --version: version information\n-r --runtime: output the time it takes for a script to execute\n");
 }
 
 int main(int argc, char** argv) {
@@ -97,34 +91,22 @@ int main(int argc, char** argv) {
     if (!lastOptionFile(argv, argv + argc, argc)) {
         printf("Error: No script specified\n");
         printUsage(argv, argc);
-        return 0;
+        return -1;
     }
 
     char const* targetFile = *(argv + (argc - 1));
 
     try {
-
-        //Load the target package
         Scribble environment(targetFile);
-
-        //Get the current time
         clock_t start = clock();
-
-        //Execute func main()
         API::APIValue val = environment.execute("main");
-
-        //Get the new current time
         clock_t end = clock();
 
         if (cmdFlagSet(argv, argv + argc, "-r")
                 || cmdFlagSet(argv, argv + argc, "--runtime")) {
-
-            //Print out the time the execution took
             printf("VM execution took time %f\n",
                    ((double) (end - start)) / (double) CLOCKS_PER_SEC);
-
         }
-
     } catch (ScribbleCore::ParserException& ex) {
         printf("Error: %s\n", ex.what());
     }
