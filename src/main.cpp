9b65ef0a83808bc814ae6878bf28307d348c1e7e
Blake Loring <blake_l@parsed.uk>
2015-02-15 16:05:38 +0000

    Fix
    BuildBot now streams logs into status endpoint rather then waiting for the full run

--- src/main.cpp
+++ src/main.cpp
@@ -87,8 +87,6 @@ int main(int argc, char** argv) {
                VERSION_BUILD_NUMBER);
         return 0;
     }
-    
-    sdadsa
 
     if (!lastOptionFile(argv, argv + argc, argc)) {
         printf("Error: No script specified\n");
